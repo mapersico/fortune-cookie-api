b9f588b7d6a8c79de58b39c8ec3dc8f1f07c2628
mapersico <marco.persico.94@gmail.com>
2024-10-11 21:39:11 -0300

    General fixes

--- src/services/chatAI.service.ts
+++ src/services/chatAI.service.ts
@@ -1,13 +1,18 @@
 import { BaseService } from "../bases";
 
 class ChatAIService extends BaseService {
-  public async getTextByPrompt(prompt: string, plainText: boolean) {
+  public async getTextByPrompt(prompt: unknown, plainText?: boolean) {
+    const parsedPrompt = (prompt as string)?.toString() || "";
+    if (!parsedPrompt) return { code: 400, content: "Prompt is required" };
+
     const result = await this.AIModel.generateContent(
-      prompt.concat(". Response must not exceed the 500 characters")
+      parsedPrompt.concat(
+        ". La respuesta no debe exceder los 500 caracteres."
+      )
     );
 
-    if (plainText) return result.response.text();
-    return result.response;
+    if (plainText) return { code: 200, content: result.response.text() };
+    return { code: 200, content: result.response };
   }
 }
 
